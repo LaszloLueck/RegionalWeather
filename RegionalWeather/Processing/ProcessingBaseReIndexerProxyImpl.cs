8ce90db8133e487f1f2a2ab7fc6824a09f594db3
Laszlo Lueck <llueck@gmail.com>
2021-04-10 00:24:22 +0200

    finished base implementation

--- RegionalWeather/Processing/ProcessingBaseReIndexerProxyImpl.cs
+++ RegionalWeather/Processing/ProcessingBaseReIndexerProxyImpl.cs
@@ -4,24 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Optional;
 using RegionalWeather.Elastic;
+using RegionalWeather.Reindexing;
 using RegionalWeather.Transport.Elastic;
 
 namespace RegionalWeather.Processing
 {
     public abstract class ProcessingBaseReIndexerProxy<T> : IProcessingBaseImplementations, IElasticConnection,
-        IOwmToElasticDocumentConverter<T>
+        IOwmToElasticDocumentConverter<T>, IDirectoryUtils
     {
         private readonly IElasticConnection _elasticConnection;
         private readonly IProcessingBaseImplementations _processingBaseImplementations;
         private readonly IOwmToElasticDocumentConverter<T> _owmToElasticDocumentConverter;
+        private readonly IDirectoryUtils _directoryUtils;
 
         protected ProcessingBaseReIndexerProxy(IElasticConnection elasticConnection,
             IProcessingBaseImplementations processingBaseImplementations,
-            IOwmToElasticDocumentConverter<T> owmToElasticDocumentConverter)
+            IOwmToElasticDocumentConverter<T> owmToElasticDocumentConverter,
+            IDirectoryUtils directoryUtils)
         {
             _elasticConnection = elasticConnection;
             _processingBaseImplementations = processingBaseImplementations;
             _owmToElasticDocumentConverter = owmToElasticDocumentConverter;
+            _directoryUtils = directoryUtils;
         }
 
         public Task<Option<X>> DeserializeObjectAsync<X>(string data) =>
@@ -54,5 +58,16 @@ namespace RegionalWeather.Processing
 
         public Task<Option<ElasticDocument>> ConvertAsync(T owmDoc) =>
             _owmToElasticDocumentConverter.ConvertAsync(owmDoc);
+
+        public bool DirectoryExists(string path) => _directoryUtils.DirectoryExists(path);
+
+        public bool CreateDirectory(string path) => _directoryUtils.CreateDirectory(path);
+
+        public IEnumerable<string> GetFilesOfDirectory(string path, string filePattern) =>
+            _directoryUtils.GetFilesOfDirectory(path, filePattern);
+
+        public IEnumerable<string> ReadAllLinesOfFile(string path) => _directoryUtils.ReadAllLinesOfFile(path);
+
+        public bool DeleteFile(string path) => _directoryUtils.DeleteFile(path);
     }
 }
--- RegionalWeather/Processing/ProcessingBaseReIndexerWeather.cs
+++ RegionalWeather/Processing/ProcessingBaseReIndexerWeather.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using RegionalWeather.Configuration;
 using RegionalWeather.Elastic;
 using RegionalWeather.Owm.CurrentWeather;
@@ -9,28 +8,14 @@ using RegionalWeather.Transport.Elastic;
 namespace RegionalWeather.Processing
 {
     public abstract class ProcessingBaseReIndexerWeather : ProcessingBaseReIndexerProxy<CurrentWeatherBase>,
-        IDirectoryUtils, IProcessingBase
+        IProcessingBase
     {
-        private readonly IDirectoryUtils _directoryUtils;
-
         protected ProcessingBaseReIndexerWeather(IElasticConnection elasticConnection,
             IOwmToElasticDocumentConverter<CurrentWeatherBase> owmDocumentConverter, IDirectoryUtils directoryUtils,
             IProcessingBaseImplementations processingBaseImplementations) : base(elasticConnection,
-            processingBaseImplementations, owmDocumentConverter)
+            processingBaseImplementations, owmDocumentConverter, directoryUtils)
         {
-            _directoryUtils = directoryUtils;
         }
-
-        public IEnumerable<string> ReadAllLinesOfFile(string path) => _directoryUtils.ReadAllLinesOfFile(path);
-
-        public bool DirectoryExists(string path) => _directoryUtils.DirectoryExists(path);
-
-        public bool CreateDirectory(string path) => _directoryUtils.CreateDirectory(path);
-
-        public IEnumerable<string> GetFilesOfDirectory(string path, string filePattern) =>
-            _directoryUtils.GetFilesOfDirectory(path, filePattern);
-
-        public bool DeleteFile(string path) => _directoryUtils.DeleteFile(path);
 
         public abstract Task Process(ConfigurationItems configuration);
     }
