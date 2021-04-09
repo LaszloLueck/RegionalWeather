e79c146e2e4c5dd58a3b9af6e02b657631657fe5
Laszlo Lueck <llueck@gmail.com>
2021-04-09 16:48:06 +0200

    make code less duplicate

---
+++ RegionalWeather/Processing/ProcessingBaseReIndexerProxyImpl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Optional;
+using RegionalWeather.Elastic;
+using RegionalWeather.Transport.Elastic;
+
+namespace RegionalWeather.Processing
+{
+    public abstract class ProcessingBaseReIndexerProxy<T> : IProcessingBaseImplementations, IElasticConnection,
+        IOwmToElasticDocumentConverter<T>
+    {
+        private readonly IElasticConnection _elasticConnection;
+        private readonly IProcessingBaseImplementations _processingBaseImplementations;
+        private readonly IOwmToElasticDocumentConverter<T> _owmToElasticDocumentConverter;
+
+        protected ProcessingBaseReIndexerProxy(IElasticConnection elasticConnection,
+            IProcessingBaseImplementations processingBaseImplementations,
+            IOwmToElasticDocumentConverter<T> owmToElasticDocumentConverter)
+        {
+            _elasticConnection = elasticConnection;
+            _processingBaseImplementations = processingBaseImplementations;
+            _owmToElasticDocumentConverter = owmToElasticDocumentConverter;
+        }
+
+        public Task<Option<X>> DeserializeObjectAsync<X>(string data) =>
+            _processingBaseImplementations.DeserializeObjectAsync<X>(data);
+
+        public ParallelQuery<X> ConvertToParallelQuery<X>(IEnumerable<X> queryable, int parallelism) =>
+            _processingBaseImplementations.ConvertToParallelQuery(queryable, parallelism);
+
+        public string BuildIndexName(string indexName, DateTime shardDatetime) =>
+            _elasticConnection.BuildIndexName(indexName, shardDatetime);
+
+        public async Task BulkWriteDocumentsAsync<X>(IEnumerable<X> documents, string indexName)
+            where X : ElasticDocument =>
+            await _elasticConnection.BulkWriteDocumentsAsync(documents, indexName);
+
+        public async Task<bool> IndexExistsAsync(string indexName) =>
+            await _elasticConnection.IndexExistsAsync(indexName);
+
+        public async Task<bool> CreateIndexAsync<X>(string indexName) where X : ElasticDocument =>
+            await _elasticConnection.CreateIndexAsync<X>(indexName);
+
+        public async Task<bool> DeleteIndexAsync(string indexName) =>
+            await _elasticConnection.DeleteIndexAsync(indexName);
+
+        public async Task<bool> RefreshIndexAsync(string indexName) =>
+            await _elasticConnection.RefreshIndexAsync(indexName);
+
+        public async Task<bool> FlushIndexAsync(string indexName) =>
+            await _elasticConnection.FlushIndexAsync(indexName);
+
+        public Task<Option<ElasticDocument>> ConvertAsync(T owmDoc) =>
+            _owmToElasticDocumentConverter.ConvertAsync(owmDoc);
+    }
+}
--- RegionalWeather/Processing/ProcessingBaseReIndexerWeather.cs
+++ RegionalWeather/Processing/ProcessingBaseReIndexerWeather.cs
@@ -1,64 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Optional;
 using RegionalWeather.Configuration;
 using RegionalWeather.Elastic;
 using RegionalWeather.Owm.CurrentWeather;
 using RegionalWeather.Reindexing;
 using RegionalWeather.Transport.Elastic;
-using Serilog;
 
 namespace RegionalWeather.Processing
 {
-    public abstract class ProcessingBaseReIndexerWeather : IProcessingBaseImplementations, IDirectoryUtils, IElasticConnection,
-        IOwmToElasticDocumentConverter<CurrentWeatherBase>, IProcessingBase
+    public abstract class ProcessingBaseReIndexerWeather : ProcessingBaseReIndexerProxy<CurrentWeatherBase>,
+        IDirectoryUtils, IProcessingBase
     {
-        private readonly IElasticConnection _elasticConnection;
-        private readonly IOwmToElasticDocumentConverter<CurrentWeatherBase> _owmDocumentConverter;
         private readonly IDirectoryUtils _directoryUtils;
-        private readonly IProcessingBaseImplementations _processingBaseImplementations;
 
         protected ProcessingBaseReIndexerWeather(IElasticConnection elasticConnection,
-            IOwmToElasticDocumentConverter<CurrentWeatherBase> owmDocumentConverter, IDirectoryUtils directoryUtils, IProcessingBaseImplementations processingBaseImplementations)
+            IOwmToElasticDocumentConverter<CurrentWeatherBase> owmDocumentConverter, IDirectoryUtils directoryUtils,
+            IProcessingBaseImplementations processingBaseImplementations) : base(elasticConnection,
+            processingBaseImplementations, owmDocumentConverter)
         {
-            _elasticConnection = elasticConnection;
-            _owmDocumentConverter = owmDocumentConverter;
             _directoryUtils = directoryUtils;
-            _processingBaseImplementations = processingBaseImplementations;
         }
 
-        public Task<Option<T>> DeserializeObjectAsync<T>(string data) =>
-            _processingBaseImplementations.DeserializeObjectAsync<T>(data);
-
-        public ParallelQuery<T> ConvertToParallelQuery<T>(IEnumerable<T> queryable, int parallelism) =>
-            _processingBaseImplementations.ConvertToParallelQuery(queryable, parallelism);
-
-        public Task<bool> RefreshIndexAsync(string indexName) => _elasticConnection.RefreshIndexAsync(indexName);
-
-        public Task<bool> FlushIndexAsync(string indexName) => _elasticConnection.FlushIndexAsync(indexName);
-
-        public string BuildIndexName(string indexName, DateTime shardDatetime) =>
-            _elasticConnection.BuildIndexName(indexName, shardDatetime);
-
         public IEnumerable<string> ReadAllLinesOfFile(string path) => _directoryUtils.ReadAllLinesOfFile(path);
-
-        public async Task<Option<ElasticDocument>> ConvertAsync(CurrentWeatherBase owmDoc) =>
-            await _owmDocumentConverter.ConvertAsync(owmDoc);
-
-        public async Task BulkWriteDocumentsAsync<T>(IEnumerable<T> documents, string indexName)
-            where T : ElasticDocument =>
-            await _elasticConnection.BulkWriteDocumentsAsync(documents, indexName);
-
-        public async Task<bool> DeleteIndexAsync(string indexName) =>
-            await _elasticConnection.DeleteIndexAsync(indexName);
-
-        public async Task<bool> IndexExistsAsync(string indexName) =>
-            await _elasticConnection.IndexExistsAsync(indexName);
-
-        public async Task<bool> CreateIndexAsync<T>(string indexName) where T : ElasticDocument =>
-            await _elasticConnection.CreateIndexAsync<WeatherLocationDocument>(indexName);
 
         public bool DirectoryExists(string path) => _directoryUtils.DirectoryExists(path);
 
