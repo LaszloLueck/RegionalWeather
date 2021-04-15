26aff75ee9d3eb6c96bd6e7a90f5d56d01dab7d1
Laszlo Lueck <llueck@gmail.com>
2021-04-15 10:23:30 +0200

    simplify startup process

--- RegionalWeather/Program.cs
+++ RegionalWeather/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
+using Optional;
 using Optional.Linq;
 using RegionalWeather.Configuration;
 using RegionalWeather.Logging;
 using RegionalWeather.Scheduler;
+using Serilog;
 
 namespace RegionalWeather
 {
@@ -20,42 +23,52 @@ namespace RegionalWeather
             await Log.InfoAsync("starting app");
 
             IConfigurationFactory configurationFactory = new ConfigurationFactory();
-            var mainTask = (await new ConfigurationBuilder(configurationFactory).GetConfigurationAsync()).Select(
-                configuration =>
+
+            var startupObjectOpt =
+                from configuration in await new ConfigurationBuilder(configurationFactory).GetConfigurationAsync()
+                from seriLogger in SerilogLoggerFactory.BuildLogger(configuration)
+                select new Tuple<ConfigurationItems, ILogger>(configuration, seriLogger);
+            
+            var mainTask = startupObjectOpt.Map(tpl =>
+            {
+                var configuration = tpl.Item1;
+                var serilogLogger = tpl.Item2;
+                var logForThisClass = serilogLogger.ForContext<Program>();
+                logForThisClass.Information("Build up the scheduler");
+
+                Task.Run(async () =>
                 {
-                    Task.Run(() =>
-                    {
-                        SerilogLoggerFactory.BuildLogger(configuration).Map(async serilogLogger =>
-                        {
-                            var logForThisClass = serilogLogger.ForContext<Program>();
-                            logForThisClass.Information("Build up the scheduler");
+                    ISchedulerFactory currentWeatherSchedulerFactory =
+                        new CustomSchedulerFactory<CurrentWeatherSchedulerJob>("currentWeatherJob",
+                            "currentWeatherGroup", "currentWeatherTrigger", 10, configuration.RunsEvery,
+                            configuration, serilogLogger);
+                    ISchedulerFactory currentWeatherReindexerFactory =
+                        new CustomSchedulerFactory<ReindexerSchedulerJobWeather>("reIndexerJob",
+                            "reIndexerGroup",
+                            "reIndexerTrigger", 5, configuration.ReindexLookupEvery, configuration,
+                            serilogLogger);
 
-                            ISchedulerFactory currentWeatherSchedulerFactory =
-                                new CustomSchedulerFactory<CurrentWeatherSchedulerJob>("currentWeatherJob",
-                                    "currentWeatherGroup", "currentWeatherTrigger", 10, configuration.RunsEvery,
-                                    configuration, serilogLogger);
-                            ISchedulerFactory currentWeatherReindexerFactory =
-                                new CustomSchedulerFactory<ReindexerSchedulerJobWeather>("reIndexerJob", "reIndexerGroup",
-                                    "reIndexerTrigger", 5, configuration.ReindexLookupEvery, configuration, serilogLogger);
+                    ISchedulerFactory airPollutionSchedulerFactory =
+                        new CustomSchedulerFactory<AirPollutionSchedulerJob>("airPollutionJob",
+                            "airPollutionGroup",
+                            "airPollutionTrigger", 15, configuration.AirPollutionRunsEvery, configuration,
+                            serilogLogger);
 
-                            ISchedulerFactory airPollutionSchedulerFactory =
-                                new CustomSchedulerFactory<AirPollutionSchedulerJob>("airPollutionJob", "airPollutionGroup",
-                                    "airPollutionTrigger", 15, configuration.AirPollutionRunsEvery, configuration, serilogLogger);
+                    ISchedulerFactory airPollutionReindexerFactory =
+                        new CustomSchedulerFactory<ReindexerSchedulerJobAirPollution>(
+                            "reindexerAirPollutionJob",
+                            "reindexerAirPollutionGroup", "reindexerAirPollutionTrigger", 15,
+                            configuration.ReindexLookupEvery, configuration, serilogLogger);
 
-                            ISchedulerFactory airPollutionReindexerFactory =
-                                new CustomSchedulerFactory<ReindexerSchedulerJobAirPollution>("reindexerAirPollutionJob",
-                                    "reindexerAirPollutionGroup", "reindexerAirPollutionTrigger", 15,
-                                    configuration.ReindexLookupEvery, configuration, serilogLogger);
+                    await currentWeatherSchedulerFactory.RunScheduler();
+                    await currentWeatherReindexerFactory.RunScheduler();
+                    await airPollutionSchedulerFactory.RunScheduler();
+                    await airPollutionReindexerFactory.RunScheduler();
+                    logForThisClass.Information("App is in running state!");
+                });
+                return Task.Delay(-1);
+            }).ValueOr(() => Task.CompletedTask);
 
-                            await currentWeatherSchedulerFactory.RunScheduler();
-                            await currentWeatherReindexerFactory.RunScheduler();
-                            await airPollutionSchedulerFactory.RunScheduler();
-                            await airPollutionReindexerFactory.RunScheduler();
-                            logForThisClass.Information("App is in running state!");
-                        });
-                    });
-                    return Task.Delay(-1);
-                }).ValueOr(() => Task.CompletedTask);
 
             await Task.WhenAll(mainTask);
             Environment.Exit(1);
